5666b65fa5d2c7c1e98784b523b2116ca942fec6
nirmalmaya <nikmayak14@gmail.com>
2018-03-08 15:45:20 +0530

    first

---
+++ src/com/fileupload/UploadServlwt.java
@@ -0,0 +1,40 @@
+package com.fileupload;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import com.oreilly.servlet.MultipartRequest;  
+
+/**
+ * Servlet implementation class UploadServlwt
+ */
+public class UploadServlwt extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public UploadServlwt() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		response.setContentType("text/html");  
+		PrintWriter out = response.getWriter();  
+		          
+		MultipartRequest m=new MultipartRequest(request,"d:/new4142");  
+		out.print("successfully uploaded");  
+
+	}
+
+}
